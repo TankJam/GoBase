d7dad9489c2dd383d2800fae64adf2898001fa37
kermitjam <1158611093@qq.com>
2021-07-20 18:24:25 +0800

    2021-07-20 18:25

--- 5、并发编程/01 goroutine 协程/01 协程基础/main.go
+++ 5、并发编程/01 goroutine 协程/01 协程基础/main.go
@@ -1,10 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 /*
 	- 启动goroutine
-	- 利用sync.WaitGroup
+	- 利用sync.WaitGroup 实现优雅等待
+		- 计数器: 结束协程的标识符
 */
 
+var wg sync.WaitGroup
+
+// hello 定义一个hello函数
+func hello(i int){
+	defer wg.Done()  // 每执行完一个协程任务，则计数器 -1
+	fmt.Println("hello tank jam!", i)
+
+}
+
 func main() {
-	
+	defer fmt.Println("哈哈哈 结束啦")
+
+	// 初始化计数器的值为10，可以允许10个协程执行
+	wg.Add(10)  // 计数器 + 10
+
+	// 创建10个协程
+	for i := 0; i < 10; i++ {
+		go hello(i)
+	}
+
+	fmt.Println("hello main init ...")
+
+	// 等待hello执行完 (执行hello函数的goroutine执行完)
+	wg.Wait()  // 阻塞态，等待所有goroutine执行结束
+	fmt.Println("所有协程执行完毕，main函数结束...")
+
 }
